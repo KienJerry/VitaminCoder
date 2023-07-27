4ed18ba6fbb77d7423db293de183881ac8ea1eb8
KienJerry <kien.nguyen@aegona.com>
2023-07-27 23:02:12 +0700

    update

--- next.config.js
+++ next.config.js
@@ -1,4 +1,26 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const path = require('path')
+const { i18n } = require('./i18n.config')
 
-module.exports = nextConfig
+module.exports = {
+  trailingSlash: true,
+  reactStrictMode: true,
+  swcMinify: false,
+  env: {
+  },
+
+  output: 'standalone',
+  images: { domains: [''] },
+  experimental: {
+    esmExternals: false,
+    jsconfigPaths: false // enables it for both jsconfig.json and tsconfig.json
+  },
+  webpack: config => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      apexcharts: path.resolve(__dirname, './node_modules/apexcharts-clevision')
+    }
+
+    return config
+  },
+  i18n,
+}
